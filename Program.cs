a36d03fd7a6863b1271addb295c3d47228334094
lamantur <Lamantur2017@gmail.com>
2023-01-06 15:29:57 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿string Prompt(string message)
+{
+    System.Console.WriteLine($"{message} >");
+    return Console.ReadLine();
+}
+
+string[] arrstr = new string[10];
+
+for (int i = 0; i < arrstr.Length; i++)
+{
+    arrstr[i] = Prompt("введите элемент массива");
+}
