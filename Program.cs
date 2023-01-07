71f3bb4c0d1759b11dec85f2836d02b6ed142296
lamantur <Lamantur2017@gmail.com>
2023-01-07 13:26:26 +0300

    Добавлены комментарии, исправления, и результат

--- Program.cs
+++ Program.cs
@@ -14,9 +14,29 @@ int PromptInt(string message)
 //Введите количество строк:
 int LengthOfArray = PromptInt("Введите количество строк: ");
 string[] arrstr = new string[LengthOfArray];
+string[] arrstrfin = new string[LengthOfArray];
 
-//Формирование стартового массива из сток
-for (int i = 0; i < arrstr.Length; i++)
+
+//Формирование стартового массива из строк и решения
+for (int i = 0; i < LengthOfArray; i++)
 {
+    
     arrstr[i] = PromptString("введите элемент массива: ");
+    if (arrstr[i].Length <=3)
+    {
+    arrstrfin[i] = arrstr[i];
+    
+    }
 }
+//Вывод только нужных элементов в правильном формате
+for (int j = 0; j < arrstrfin.Length; j++)
+{
+   if(string.IsNullOrEmpty(arrstrfin[j])) 
+   {
+    System.Console.Write("");
+   }else{
+System.Console.Write(arrstrfin[j] + "; ");
+   }
+   
+}
+
